8efd01e575d24bf9e590e2f7a9d58e535056828c
Maximilian Hofer <maximilian@alexxdesign.at>
2017-12-03 18:37:02 +0100

    Update Recursion&Lists.pl
    Correcting the wrong names.

--- Recursion&Lists.pl
+++ Recursion&Lists.pl
@@ -1,7 +1,7 @@
 [user].
-pushFront(H, T,[H|T]).
-pushEnd(H,[ ],[H]).
-pushEnd(X,[H|T],[H|NT]) :- pushEnd(X,T,NT).
-list_length([_|T],L) :- list_length(T,N),L is N+1 .
-del([H|T], H, T).
-del([H|T], X, [H|NT]) :- del(T, X, NT).
+prepend(H, T,[H|T]).
+addElement(H,[ ],[H]).
+addElement(X,[H|T],[H|NT]) :- pushEnd(X,T,NT).
+hasLength([_|T],L) :- list_length(T,N),L is N+1 .
+remove([H|T], H, T).
+remove([H|T], X, [H|NT]) :- del(T, X, NT).
